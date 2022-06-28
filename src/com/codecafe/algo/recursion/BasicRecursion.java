af1fc9fadc20c20f22cae21fe4c79cc89bcbe8b3
Abhinav Nath <abhinavnath@ymail.com>
2022-06-28 23:45:46 +0530

    Add basic example of Recursion

---
+++ src/com/codecafe/algo/recursion/BasicRecursion.java
@@ -0,0 +1,22 @@
+package com.codecafe.algo.recursion;
+
+public class BasicRecursion {
+
+  public static void main(String[] args) {
+    printNumber(5);
+  }
+
+  private static void printNumber(int n) {
+    if (n == 0)
+      return;
+
+    System.out.print(n + " ");
+    printNumber(n - 1);
+  }
+
+}
+
+/*
+  Output:
+  5 4 3 2 1
+*/
