8ac7b11feff5fc5e8cb05dcd80717396d257e32d
Abhinav Nath <abhinavnath@ymail.com>
2022-09-11 21:52:26 +0530

    Add parametrized test for StringReversal

--- src/main/java/com/codecafe/algorithms/recursion/StringReversal.java
+++ src/main/java/com/codecafe/algorithms/recursion/StringReversal.java
@@ -2,12 +2,7 @@ package com.codecafe.algorithms.recursion;
 
 public class StringReversal {
 
-  public static void main(String[] args) {
-    String str = "hello";
-    System.out.println(reverse(str));
-  }
-
-  private static String reverse(String str) {
+  public String reverse(String str) {
 
     // base case
     if ("".equals(str))
